3f9fdaa6cd2bfb3749da4e36bb5203e6ee34708b
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-03-02 10:35:41 +0300

    edit methotları eklendi

--- CetStudentBook/Controllers/BooksController.cs
+++ CetStudentBook/Controllers/BooksController.cs
@@ -37,4 +37,37 @@ public class BooksController : Controller
         }
         return View(book);
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var book = _context.Books.Find(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return View(book);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Book book)
+    {
+        if (id != book.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(book);
+    }
 }
