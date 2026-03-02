070f19421fcb054414ca6cf88988a3e7021cf118
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-03-02 10:04:00 +0300

    veritabanı ile arayüz arası köprü oluşturuldu yani bookscontroller

---
+++ CetStudentBook/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+using CetStudentBook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CetStudentBook.Controllers;
+
+public class BooksController : Controller
+
+{
+    private readonly ApplicationDbContext _context;
+    
+    public BooksController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public IActionResult Index()
+    {
+        var books = _context.Books.ToList();
+        return View();
+    }
+}
